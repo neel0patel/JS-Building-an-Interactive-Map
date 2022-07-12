b350e0780bf9fd38048735b5adc735876f7c8e35
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-07-12 18:58:49 -0400

    Interactive Map

--- public/app.js
+++ public/app.js
@@ -1,19 +1,46 @@
 // create map
-
+var map = L.map("map").setView([51.505, -0.09], 13);
 
 // add openstreetmap tiles
-
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
 
 // create and main add geolocation marker
-
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 // draw the 2nd arrondissement
-
-
+var circle = L.circle([51.508, -0.11], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 500,
+}).addTo(map);
 // create red pin marker
-
+var circle = L.circle([51.508, -0.11], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 500,
+}).addTo(map);
 
 // metro station markers
+var polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047],
+]).addTo(map);
 
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
+var popup = L.popup();
 
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
+}
+
+map.on("click", onMapClick);
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const app = express()
-const liveServer = require('live-server')
+const express = require("express");
+const app = express();
+const liveServer = require("live-server");
 
 async function main() {
-
-    app.listen(3001, () => {
-        liveServer.start({
-            port: 3000,
-            logLevel: 0,
-            root: './public'
-        })
-    })
+  app.listen(3001, () => {
+    liveServer.start({
+      port: 3000,
+      logLevel: 0,
+      root: "./public",
+    });
+  });
 }
 
-main()
+main();
